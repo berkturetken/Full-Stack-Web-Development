74a91c03a6c7cd7773e5ef570bad9516741a4e10
Berk Türetken <berkturetken1997@hotmail.com>
2022-12-06 23:32:21 +0200

    Implemented 2.15

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -34,10 +34,15 @@ const App = () => {
         name: newName,
         number: newNumber,
       };
-      setPersons(persons.concat(newPerson));
+
+      axios
+        .post("http://localhost:3001/persons", newPerson)
+        .then((response) => {
+          setPersons(persons.concat(response.data));
+          setNewName("");
+          setNewNumber("");
+        });
     }
-    setNewName("");
-    setNewNumber("");
   };
 
   const handleNameChange = (event) => setNewName(event.target.value);
