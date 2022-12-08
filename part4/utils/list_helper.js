e6367bd2503ab3029387e5c90ca8ce2cdba80c17
Berk Türetken <berkturetken1997@hotmail.com>
2022-12-09 00:57:06 +0200

    Implemented 4.5

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -9,7 +9,20 @@ const totalLikes = (blogs) => {
   return blogs.reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  const reducer = (max, blog) => {
+    return max.likes > blog.likes ? max : blog
+  }
+  const favBlog = blogs.reduce(reducer, 0)
+  return {
+    title: favBlog.title,
+    author: favBlog.author,
+    likes: favBlog.likes,
+  }
+}
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 }
