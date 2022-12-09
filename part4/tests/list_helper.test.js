4a85b92d3afa26c9adab382d5043d6aa41d22e4a
Berk Türetken <berkturetken1997@hotmail.com>
2022-12-09 02:11:27 +0200

    Implemented 4.7

--- part4/tests/list_helper.test.js
+++ part4/tests/list_helper.test.js
@@ -136,3 +136,26 @@ describe('most blogs', () => {
     })
   })
 })
+
+describe('most likes', () => {
+  test('of empty list is an empty blog', () => {
+    const result = listHelper.mostLikes(emptyList)
+    expect(result).toEqual({})
+  })
+
+  test('when list has only one blog, equals that blog', () => {
+    const result = listHelper.mostLikes(listWithOneBlog)
+    expect(result).toEqual({
+      author: 'Edsger W. Dijkstra',
+      likes: 5,
+    })
+  })
+
+  test('of a bigger list is found right', () => {
+    const result = listHelper.mostLikes(listWithMultipleBlogs)
+    expect(result).toEqual({
+      author: 'Edsger W. Dijkstra',
+      likes: 17,
+    })
+  })
+})
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -25,18 +25,46 @@ const favoriteBlog = (blogs) => {
 
 const mostBlogs = (blogs) => {
   const authorsAndBlogNumbers = lodash.countBy(blogs, 'author')
-  let mostBlogNumber = Math.max(...Object.values(authorsAndBlogNumbers))
-  const topAuthor = Object.keys(authorsAndBlogNumbers).find(
-    (key) => authorsAndBlogNumbers[key] === mostBlogNumber
-  )
 
-  mostBlogNumber =
-    mostBlogNumber === Number.NEGATIVE_INFINITY ? undefined : mostBlogNumber
+  if (
+    authorsAndBlogNumbers &&
+    Object.keys(authorsAndBlogNumbers).length !== 0
+  ) {
+    let mostBlogNumber = Math.max(...Object.values(authorsAndBlogNumbers))
+    const topAuthor = Object.keys(authorsAndBlogNumbers).find(
+      (key) => authorsAndBlogNumbers[key] === mostBlogNumber
+    )
 
-  return {
-    author: topAuthor,
-    blogs: mostBlogNumber,
+    return {
+      author: topAuthor,
+      blogs: mostBlogNumber,
+    }
   }
+  return {}
+}
+
+const mostLikes = (blogs) => {
+  let authorsAndLikes = {}
+  blogs.forEach((blog) => {
+    if (authorsAndLikes[blog.author]) {
+      authorsAndLikes[blog.author] += blog.likes
+    } else {
+      authorsAndLikes[blog.author] = 0 + blog.likes
+    }
+  })
+
+  if (authorsAndLikes && Object.keys(authorsAndLikes).length !== 0) {
+    let mostBlogLikes = Math.max(...Object.values(authorsAndLikes))
+    const topAuthor = Object.keys(authorsAndLikes).find(
+      (key) => authorsAndLikes[key] === mostBlogLikes
+    )
+
+    return {
+      author: topAuthor,
+      likes: mostBlogLikes,
+    }
+  }
+  return {}
 }
 
 module.exports = {
@@ -44,4 +72,5 @@ module.exports = {
   totalLikes,
   favoriteBlog,
   mostBlogs,
+  mostLikes,
 }
