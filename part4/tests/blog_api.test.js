c7862a2b7c3be038ee76a199ae1bf7c1323ab3c9
Berk Türetken <berkturetken1997@hotmail.com>
2022-12-09 16:30:29 +0200

    Implemented 4.10

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -33,6 +33,27 @@ test('the unique identifier property of the blog posts is named id exists', asyn
   }
 })
 
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    title: 'A New Book',
+    author: 'Hakan Türetken',
+    url: 'https://www.google.com/',
+    likes: 7,
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await helper.notesInDb()
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+
+  const contents = blogsAtEnd.map((n) => n.title)
+  expect(contents).toContain('A New Book')
+})
+
 afterAll(() => {
   mongoose.connection.close()
 })
--- part4/tests/test_helper.js
+++ part4/tests/test_helper.js
@@ -1,4 +1,4 @@
-// const Blog = require('../models/blog')
+const Blog = require('../models/blog')
 
 const initialBlogs = [
   {
@@ -15,6 +15,12 @@ const initialBlogs = [
   },
 ]
 
+const notesInDb = async () => {
+  const blogs = await Blog.find({})
+  return blogs.map((note) => note.toJSON())
+}
+
 module.exports = {
   initialBlogs,
+  notesInDb,
 }
