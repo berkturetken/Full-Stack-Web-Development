f5c8bfc19772f531fd2f745b51b894d9b0cfb787
Berk Türetken <berkturetken1997@hotmail.com>
2022-12-09 16:39:32 +0200

    Implemented 4.11

--- part4/models/blog.js
+++ part4/models/blog.js
@@ -7,4 +7,15 @@ const blogSchema = new mongoose.Schema({
   likes: Number,
 })
 
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    if (returnedObject.likes === undefined) {
+      returnedObject.likes = 0
+    }
+    // returnedObject.id = returnedObject._id.toString()
+    // delete returnedObject._id
+    // delete returnedObject.__v
+  },
+})
+
 module.exports = mongoose.model('Blog', blogSchema)
--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -16,13 +16,13 @@ beforeEach(async () => {
   }
 })
 
-test('all blogs are returned', async () => {
+test('apiTest - all blogs are returned', async () => {
   const response = await api.get('/api/blogs')
 
   expect(response.body).toHaveLength(helper.initialBlogs.length)
 })
 
-test('the unique identifier property of the blog posts is named id exists', async () => {
+test('apiTest - the unique identifier property of the blog posts is named id exists', async () => {
   const response = await api
     .get('/api/blogs')
     .expect(200)
@@ -33,7 +33,7 @@ test('the unique identifier property of the blog posts is named id exists', asyn
   }
 })
 
-test('a valid blog can be added', async () => {
+test('apiTest - a valid blog can be added', async () => {
   const newBlog = {
     title: 'A New Book',
     author: 'Hakan Türetken',
@@ -52,6 +52,22 @@ test('a valid blog can be added', async () => {
 
   const contents = blogsAtEnd.map((n) => n.title)
   expect(contents).toContain('A New Book')
+})
+
+test('apiTest - blog without likes can be added by setting the likes to 0', async () => {
+  const newBlog = {
+    title: 'A New Book',
+    author: 'Hakan Türetken',
+    url: 'https://www.google.com/',
+  }
+
+  const response = await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  expect(response.body.likes).toBe(0)
 })
 
 afterAll(() => {
