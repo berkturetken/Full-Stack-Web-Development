98bf09184840b607a4dcd359127be4e9782f1f2f
Berk Türetken <berkturetken1997@hotmail.com>
2022-12-24 15:27:18 +0200

    Implemented 5.18

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,6 +1,12 @@
 describe('Blog app', function () {
   beforeEach(function () {
     cy.request('POST', 'http://localhost:8080/api/testing/reset')
+    const user = {
+      name: 'Cypress Test User',
+      username: 'cypressTest',
+      password: 'cypress',
+    }
+    cy.request('POST', 'http://localhost:8080/api/users/', user)
     cy.visit('http://localhost:3000')
   })
 
@@ -8,5 +14,22 @@ describe('Blog app', function () {
     cy.contains('Log in to application')
     cy.contains('username')
     cy.contains('password')
+  })
+
+  describe('Login', function () {
+    it.only('succeeds with correct credentials', function () {
+      cy.get('#username').type('cypressTest')
+      cy.get('#password').type('cypress')
+      cy.get('#login-button').click()
+      cy.contains('Cypress Test User logged in')
+    })
+
+    it.only('fails with wrong credentials', function () {
+      cy.get('#username').type('userDoesNotExist')
+      cy.get('#password').type('wrong')
+      cy.get('#login-button').click()
+      cy.get('.error').contains('wrong username or password')
+      cy.get('html').should('not.contain', 'Ata Metin Türetken logged in')
+    })
   })
 })
